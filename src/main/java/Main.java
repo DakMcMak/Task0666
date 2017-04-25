4b1787d770636f8aa67506c34414a5cecf2070f1
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-25 23:43:12 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+    public class Main
+    {
+        private static int catCount = 0;
+
+        public static void main(String[] args) throws Exception
+        {
+            Cat vaska = new Cat("Bella");
+            Cat murka = new Cat("Tiger");
+
+            System.out.println("Cat count " + catCount);
+        }
+
+        public static class Cat
+        {
+            private String name;
+
+            public Cat(String name)
+            {
+                this.name = name;
+                Main.catCount++;
+            }
+        }
+
+    }
